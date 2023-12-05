024f9bf8040f1e4f8b688b40ef7bc5bfe474aa7e
Elias <epertt@gmail.com>
2023-12-05 04:28:23 +0200

    remove development comments

--- src/ui/calendar_view.py
+++ src/ui/calendar_view.py
@@ -67,39 +67,28 @@ class CalendarView():
     def _display_months(self, month_range=range(1, 13)):
         row = col = 0
 
-        # fixed size for each month frame; though not ideal, still looks better
-        # than having mismatched heights for months
         month_frame_width = 200
         month_frame_height = 200
 
         for month in month_range:
             month_calendar = calendar.monthcalendar(self._current_year, month)
 
-            # a separate frame for each month is needed to use grid
             month_frame = ttk.Frame(self._calendar_frame, borderwidth=1, relief="solid",
                                     width=month_frame_width, height=month_frame_height,
                                     padding=(3, 15, 0, 0))
             month_frame.grid(row=row, column=col, padx=10, pady=10)
             month_frame.grid_propagate(False)
 
-            # month name as a button
             # TODO: clicking one of these should open a month view for that month
             month_name = calendar.month_name[month]
             month_button = ttk.Button(month_frame, text=month_name)
             month_button.grid(row=0, column=0, columnspan=7, pady=(0, 15))
 
-            # under the month name there should be the weekdays (abbreviated so they
-            # don't take too much space)
             for day_name in calendar.day_name:
                 day_label = ttk.Label(month_frame, text=day_name[:3], width=-3)
                 day_label.grid(row=1, column=list(
                     calendar.day_name).index(day_name), pady=(0, 5))
 
-            # under the abbreviated weekdays there should be a grid of days numbered from
-            # 1 to 28-31, depending on how many days the month has; the calendar module
-            # gives days outside the given month as "0", which can be ignored
-            # label instead of button seems necessary here for style reasons and for
-            # properly fitting on the grid
             for week in month_calendar:
                 for day in week:
                     if day > 0:
@@ -109,7 +98,6 @@ class CalendarView():
                         else:
                             day_label = ttk.Label(month_frame, text=str(
                                 day), cursor="hand2", width=-2)
-                        # the user should be able to click any given day to see, add or delete notes
                         day_label.grid(row=month_calendar.index(
                             week) + 2, column=week.index(day))
                         day_label.bind("<Button-1>", lambda event, y=self._current_year,
@@ -131,20 +119,15 @@ class CalendarView():
         self._show_calendar_view()
 
     def _init(self):
-        # frame for menus (configure, add/remove notes, help, logout)
         self._menu_frame = ttk.Frame(self._root, padding=10)
 
-        # main container frame
         self._main_frame = ttk.Frame(self._root, padding=10)
 
         self.pack()
 
-        # frame that contains buttons like so: < {year} >
-        # for going forward or back in time
         self._year_menu_frame = ttk.Frame(self._main_frame)
         self._year_menu_frame.pack()
 
-        # frame that contains the calendar itself
         self._calendar_frame = ttk.Frame(self._main_frame)
         self._calendar_frame.pack()
 
